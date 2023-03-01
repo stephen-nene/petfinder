526101d563ae4607e9919ea925bad94ee2a2ab62
+stephen.nene <+>
2023-03-01 15:17:22 +0300

    at log in page

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import Login from '../components/login/Login';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
